4745c0333e8129f07c6dab823d4b298f8e9876e6
adarshsa <adarshsa@thoughtworks.com>
2018-10-20 23:58:45 +0530

    added two more number problem -- * finding a integer with odd occurrence * changing a given binary list to integer

--- src/main/java/NumberProblems.java
+++ src/main/java/NumberProblems.java
@@ -1,5 +1,40 @@
-public class NumberProblems {
-    public static int oddCount(int number) {
-       return number/2;
+import java.util.*;
+import java.util.stream.IntStream;
+
+class NumberProblems {
+
+    static int oddCount(int number) {
+        return number / 2;
+    }
+
+
+    // assuming there will be only one integer with odd occurrence
+    
+    static int findIntOfOddOccurrence(int[] integers) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        Arrays.stream(integers)
+                .boxed()
+                .forEach(number -> {
+                    if (map.containsKey(number)) {
+                        Integer value = map.get(number);
+                        map.put(number, ++value);
+                    }
+                    map.putIfAbsent(number, 1);
+                });
+
+        return map.entrySet()
+                .stream()
+                .filter(key -> key.getValue() % 2 != 0).map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(0);
+    }
+
+    static int ConvertBinaryArrayToInt(List<Integer> binary) {
+        Collections.reverse(binary);
+        return (int) IntStream.range(0, binary.size())
+                .filter(e -> binary.get(e) != 0)
+                .mapToDouble(i -> Math.pow(2, i))
+                .sum();
     }
 }
--- test/NumberProblemsTest.java
+++ test/NumberProblemsTest.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class NumberProblemsTest {
@@ -8,4 +11,21 @@ public class NumberProblemsTest {
         assertEquals(2, NumberProblems.oddCount(5));
     }
 
+    @Test
+    public void should_find_the_number_of_odd_occurrence_in_given_array() {
+        assertEquals(5, NumberProblems.findIntOfOddOccurrence(new int[]{20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5}));
+        assertEquals(5, NumberProblems.findIntOfOddOccurrence(new int[]{20,1,1,2,2,3,3,5,5,4,20,4,5}));
+        assertEquals(10, NumberProblems.findIntOfOddOccurrence(new int[]{10}));
+        assertEquals(10, NumberProblems.findIntOfOddOccurrence(new int[]{1,1,1,1,1,1,10,1,1,1,1}));
+        assertEquals(1, NumberProblems.findIntOfOddOccurrence(new int[]{5,4,3,2,1,5,4,3,2,10,10}));
+    }
+
+    @Test
+    public void should_return_number_representation_of_given_binary() throws Exception {
+
+        assertEquals(1, NumberProblems.ConvertBinaryArrayToInt(new ArrayList<>(Arrays.asList(0,0,0,1))));
+        assertEquals(15, NumberProblems.ConvertBinaryArrayToInt(new ArrayList<>(Arrays.asList(1,1,1,1))));
+        assertEquals(6, NumberProblems.ConvertBinaryArrayToInt(new ArrayList<>(Arrays.asList(0,1,1,0))));
+        assertEquals(9, NumberProblems.ConvertBinaryArrayToInt(new ArrayList<>(Arrays.asList(1,0,0,1))));
+    }
 }
